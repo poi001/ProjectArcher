0e71de70f943efdd0718712704e168b644e5dcee
poi001 <107660181+poi001@users.noreply.github.com>
2023-11-07 22:22:59 +0900

    First

---
+++ Source/ProjectArcher/ProjectArcherGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectArcherGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTARCHER_API AProjectArcherGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
